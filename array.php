560c9ad5fd2f624211ad8ebe8e91740f61ed3e02
Abhishekn1234 <111786351+Abhishekn1234@users.noreply.github.com>
2023-09-23 20:55:01 +0530

    Add files via upload

--- array.php
+++ array.php
@@ -1,36 +1,39 @@
 <?php
 
-function rotateMatrix($matrix)
+// This function stores transpose
+// of A[][] in B[][]
+function transpose(&$A, &$B)
 {
-    $rows = count($matrix);
-    $columns = count($matrix[0]);
-    
-    $rotatedMatrix = array();
-    for ($i = 0; $i < $columns; $i++) {
-        $rotatedMatrix[$i] = array();
-        for ($j = 0; $j < $rows; $j++) {
-            $rotatedMatrix[$i][$j] = $matrix[$j][$columns - $i - 1];
-        }
-    }
-    
-    return $rotatedMatrix;
+	$N = 6;
+	for ($i = 0; $i < $N; $i++)
+		for ($j = 0; $j < $N; $j++)
+			$B[$i][$j] = $A[$j][$i];
 }
 
-$inputMatrix = array(
-    array(1, 2, 3, 4, 5, 6),
-    array(7, 8, 9, 10, 11, 12),
-    array(13, 14, 15, 16, 17, 18),
-    array(19, 20, 21, 22, 23, 24),
-    array(25, 26, 27, 28, 29, 30),
-    array(31, 32, 33, 34, 35, 36)
-);
 
-$rotatedMatrix = rotateMatrix($inputMatrix);
 
-foreach ($rotatedMatrix as $row) {
-    foreach ($row as $value) {
-        echo $value . " ";
-    }
-    echo "\n";
+$A = array(array(1, 2, 3, 4,5,6),
+		array( 7, 8 , 9, 10, 11,12),
+		array(13, 14, 15, 16, 17, 18),
+		array(19 , 20, 21, 22, 23, 24),
+        array(25,26,27,28,29,30),
+        array(31,32,33,34,35,36));
+
+$N = 6;
+
+
+transpose($A, $B);
+
+echo "Result matrix is \n";
+for ($i = 0; $i < $N; $i++)
+{
+	for ($j = 0; $j < $N; $j++)
+	{
+	echo $B[$i][$j];
+	echo " ";
+	}
+	echo "\n";
 }
+
+
 ?>
